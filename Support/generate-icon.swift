964987b7f94892787121c36ea1c67240ad26b454
krabople <166812596+krabople@users.noreply.github.com>
2026-09-06 08:54:30 +0100

    Generate exact required iOS icon sizes

--- Support/generate-icon.swift
+++ Support/generate-icon.swift
@@ -53,29 +53,55 @@ else {
 }
 
 let colorSpace = CGColorSpaceCreateDeviceRGB()
-guard let cgContext = CGContext(
-    data: nil,
-    width: 1024,
-    height: 1024,
-    bitsPerComponent: 8,
-    bytesPerRow: 1024 * 4,
-    space: colorSpace,
-    bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue
-) else {
-    fatalError("Unable to create opaque CGContext")
-}
-
-cgContext.draw(cgImage, in: CGRect(x: 0, y: 0, width: 1024, height: 1024))
-
-guard
-    let opaqueCgImage = cgContext.makeImage(),
-    let data = NSBitmapImageRep(cgImage: opaqueCgImage).representation(using: NSBitmapImageRep.FileType.png, properties: [:])
-else {
-    fatalError("Unable to generate opaque PNG data")
-}
-
 let outputURL = URL(fileURLWithPath: output)
-try FileManager.default.createDirectory(at: outputURL.deletingLastPathComponent(), withIntermediateDirectories: true)
-try data.write(to: outputURL, options: Data.WritingOptions.atomic)
-print("Generated opaque icon at \(outputURL.path)")
+let outputDirectory = outputURL.deletingLastPathComponent()
+try FileManager.default.createDirectory(at: outputDirectory, withIntermediateDirectories: true)
+
+let variants: [(filename: String, pixels: Int)] = [
+    (outputURL.lastPathComponent, 1024),
+    ("MorrowIcon-20.png", 20),
+    ("MorrowIcon-20@2x.png", 40),
+    ("MorrowIcon-20@3x.png", 60),
+    ("MorrowIcon-29.png", 29),
+    ("MorrowIcon-29@2x.png", 58),
+    ("MorrowIcon-29@3x.png", 87),
+    ("MorrowIcon-40.png", 40),
+    ("MorrowIcon-40@2x.png", 80),
+    ("MorrowIcon-40@3x.png", 120),
+    ("MorrowIcon-60@2x.png", 120),
+    ("MorrowIcon-60@3x.png", 180),
+    ("MorrowIcon-76.png", 76),
+    ("MorrowIcon-76@2x.png", 152),
+    ("MorrowIcon-83.5@2x.png", 167),
+]
+
+for variant in variants {
+    guard let context = CGContext(
+        data: nil,
+        width: variant.pixels,
+        height: variant.pixels,
+        bitsPerComponent: 8,
+        bytesPerRow: variant.pixels * 4,
+        space: colorSpace,
+        bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue
+    ) else {
+        fatalError("Unable to create opaque CGContext for \(variant.filename)")
+    }
+
+    context.interpolationQuality = .high
+    context.draw(cgImage, in: CGRect(x: 0, y: 0, width: variant.pixels, height: variant.pixels))
+
+    guard
+        let renderedImage = context.makeImage(),
+        let data = NSBitmapImageRep(cgImage: renderedImage).representation(
+            using: NSBitmapImageRep.FileType.png,
+            properties: [:]
+        )
+    else {
+        fatalError("Unable to generate opaque PNG data for \(variant.filename)")
+    }
+
+    try data.write(to: outputDirectory.appendingPathComponent(variant.filename), options: .atomic)
+    print("Generated \(variant.filename) at \(variant.pixels)x\(variant.pixels)")
+}
 
